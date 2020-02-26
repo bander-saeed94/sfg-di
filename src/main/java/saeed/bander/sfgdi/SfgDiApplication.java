af9b1c53872863d5a20299668f46982e4737eadc
Bander Alshammari <bannadr1@hotmail.com>
2020-02-26 23:10:15 +0300

    Profiles

--- src/main/java/saeed/bander/sfgdi/SfgDiApplication.java
+++ src/main/java/saeed/bander/sfgdi/SfgDiApplication.java
@@ -13,6 +13,8 @@ public class SfgDiApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
 
 		System.out.println("----- Primary");
 		MyController myController = (MyController) ctx.getBean("myController");
